87d45a1a7067d0d58b2443f60500b70ec4ba6360
And_Ocean <and_ocean@163.com>
2024-11-02 20:45:30 +0800

    init server

---
+++ scu_bms_server/src/main/java/com/example/scu_bms_server/ScuBmsServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.scu_bms_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScuBmsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScuBmsServerApplication.class, args);
+    }
+
+}
---
+++ scu_bms_server/src/test/java/com/example/scu_bms_server/ScuBmsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scu_bms_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScuBmsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
